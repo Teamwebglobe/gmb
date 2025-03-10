06b6124e9e5f69e3abb5ac7416d9a15cca1da1d4
Nitesh-Mann <maannitesh13@gmail.com>
2025-03-10 14:16:08 +0530

    loader

---
+++ script/index.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Scroll to top on page refresh
+  window.scrollTo(0, 0);
+
+  let countElement = document.getElementById("count");
+  let progressBar = document.querySelector(".progress-bar");
+  let loaderWrapper = document.querySelector(".loader-wrapper");
+
+  let loadStartTime = performance.now(); // Get load start time
+
+  window.onload = function () {
+    let loadEndTime = performance.now(); // Get load end time
+    let loadDuration = loadEndTime - loadStartTime; // Calculate load duration in milliseconds
+
+    let totalProgress = 100;
+    let intervalTime = Math.max(loadDuration / totalProgress, 40); // Set a minimum interval time to avoid excessive speed
+
+    let loadProgress = 0;
+    let interval = setInterval(() => {
+      loadProgress += 1;
+      countElement.textContent =
+        loadProgress < 10 ? "0" + loadProgress : loadProgress;
+      progressBar.style.width = loadProgress + "%";
+
+      if (loadProgress >= totalProgress) {
+        clearInterval(interval);
+        setTimeout(() => {
+          loaderWrapper.classList.add("hide-loader"); // Add class instead of display: none
+        }, 500); // Delay before hiding the loader
+      }
+    }, intervalTime);
+  };
+});
