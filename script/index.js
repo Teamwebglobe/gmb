5728c6c4edf11f41820115732574e707dfeb78dd
Nitesh-Mann <maannitesh13@gmail.com>
2025-03-10 17:21:15 +0530

    loader

--- script/index.js
+++ script/index.js
@@ -1,36 +1,24 @@
 // loader //
-document.addEventListener("DOMContentLoaded", function () {
-  // Scroll to top on page refresh
-  window.scrollTo(0, 0);
 
+document.addEventListener("DOMContentLoaded", function () {
   let countElement = document.getElementById("count");
   let progressBar = document.querySelector(".progress-bar");
   let loaderWrapper = document.querySelector(".loader-wrapper");
 
-  let loadStartTime = performance.now(); // Get load start time
+  let loadProgress = 0;
+  let interval = setInterval(() => {
+    loadProgress += 1;
+    countElement.textContent =
+      loadProgress < 10 ? "0" + loadProgress : loadProgress;
+    progressBar.style.width = loadProgress + "%";
 
-  window.onload = function () {
-    let loadEndTime = performance.now(); // Get load end time
-    let loadDuration = loadEndTime - loadStartTime; // Calculate load duration in milliseconds
-
-    let totalProgress = 100;
-    let intervalTime = Math.max(loadDuration / totalProgress, 40); // Set a minimum interval time to avoid excessive speed
-
-    let loadProgress = 0;
-    let interval = setInterval(() => {
-      loadProgress += 1;
-      countElement.textContent =
-        loadProgress < 10 ? "0" + loadProgress : loadProgress;
-      progressBar.style.width = loadProgress + "%";
-
-      if (loadProgress >= totalProgress) {
-        clearInterval(interval);
-        setTimeout(() => {
-          loaderWrapper.classList.add("hide-loader"); // Add class instead of display: none
-        }, 500); // Delay before hiding the loader
-      }
-    }, intervalTime);
-  };
+    if (loadProgress >= 100) {
+      clearInterval(interval);
+      setTimeout(() => {
+        loaderWrapper.classList.add("hide-loader"); //
+      }, 800); // Delay before hiding the loader
+    }
+  }, 50);
 });
 
 //header//
