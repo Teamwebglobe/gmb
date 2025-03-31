93d7a10628c0bd1f75a53739f3a7ba697d37e85e
Nitesh-Mann <maannitesh13@gmail.com>
2025-03-31 16:03:58 +0530

    form

--- sendmail.php
+++ sendmail.php
@@ -1,16 +1,21 @@
 <?php
-if (isset($_POST['sub'])) {
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['sub'])) {
     // Fetching form data
-    $firstName = $_POST['inputFirstName'];
-    $lastName = $_POST['lastFirstName'];
-    $company = $_POST['inputCompanyName'];
-    $email = $_POST['inputEmail4'];
+    $firstName = isset($_POST['inputFirstName']) ? $_POST['inputFirstName'] : '';
+    $lastName = isset($_POST['lastFirstName']) ? $_POST['lastFirstName'] : '';
+    $company = isset($_POST['inputCompanyName']) ? $_POST['inputCompanyName'] : '';
+    $email = isset($_POST['inputEmail4']) ? $_POST['inputEmail4'] : '';
     $message = isset($_POST['flaotingTextarea2']) ? $_POST['flaotingTextarea2'] : '';
+
+    // Validate email
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        echo "<script>alert('Invalid email format!'); window.location.href='index.html';</script>";
+        exit;
+    }
 
     // Email recipient
     $to = "maannitesh13@gmail.com"; 
     $bcc = "jitenderbachhraj5@gmail.com"; 
-    
     $subject = "New Contact Form Submission";
     
     // Email content
@@ -20,19 +25,15 @@ if (isset($_POST['sub'])) {
     $email_message .= "Message: $message\n";
 
     // Email headers
-    $headers = [
-        "MIME-Version: 1.0",
-        "Content-type: text/plain; charset=utf-8",
-        "From: no-reply@yourdomain.com",
-        "Bcc: $bcc"
-    ];
-    
-    // Convert headers to string
-    $headers = implode("\r\n", $headers);
+    $headers = "MIME-Version: 1.0" . "\r\n";
+    $headers .= "Content-type: text/plain; charset=utf-8" . "\r\n";
+    $headers .= "From: no-reply@yourdomain.com" . "\r\n";
+    $headers .= "Bcc: $bcc" . "\r\n";
 
     // Send the email
     if (mail($to, $subject, $email_message, $headers)) {
-        echo "<script>window.location.href='thanku.html';</script>";
+        header("Location: thanku.html");
+        exit;
     } else {
         echo "<script>alert('Message could not be sent.'); window.location.href='index.html';</script>";
     }
